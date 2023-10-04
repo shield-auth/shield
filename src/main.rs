8cd646da99e4eb6325459fa35cff93d162193c94
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2023-10-05 03:00:32 +0530

    stale constants removed

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,6 @@ use project::{index as project_index};
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello World! I'm Shield"
-}
-
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build()
