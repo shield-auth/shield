2a0c000a394e74ac88a6c11a31b636543c6011e3
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2023-10-05 02:28:17 +0530

    folder structure created and routes configured (#7)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
+mod constants;
+mod routes;
+mod organisation;
+mod project;
+
+use routes::{get_routes, RouteKey};
+use organisation::{index as org_index};
+use project::{index as project_index};
+
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello World! I'm Shield"
-}
-
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build()
+        .mount("/", routes![index])
+        .mount(&get_path(&RouteKey::Organisations), routes![org_index])
+        .mount(&get_path(&RouteKey::Projects), routes![project_index]);
 
     Ok(rocket.into())
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello World! It's your Shield"
+}
+
+fn get_path(route: &RouteKey) -> String {
+    let routes_map = get_routes();
+    routes_map.get(route).map(|s|s.to_string()).expect("Route not found")
 }
