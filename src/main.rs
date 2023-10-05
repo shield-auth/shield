e85b27efd2c34c0872bc59e265e617aad2a11672
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2023-10-06 04:29:28 +0530

    Develop (#13)
    * folder structure created and routes configured
    
    * stale constants removed
    
    * stale constants removed
    
    * cache cleared
    
    * duplicate function removed
    
    * Updated README.md
    
    * framework changed to actix-web
    
    * accidental removal restored

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,21 @@
-mod routes;
+use actix_web::{get, web::{ServiceConfig, scope}};
+use shuttle_actix_web::ShuttleActixWeb;
+use crate::organisation::factory::organisation_factory;
+
 mod organisation;
 mod project;
 
-use routes::{get_routes, RouteKey};
-use organisation::{index as org_index};
-use project::{index as project_index};
-
-#[macro_use]
-extern crate rocket;
-
-#[shuttle_runtime::main]
-async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build()
-        .mount("/", routes![index])
-        .mount(&get_path(&RouteKey::Organisations), routes![org_index])
-        .mount(&get_path(&RouteKey::Projects), routes![project_index]);
-    Ok(rocket.into())
-}
-
-#[get("/")]
-fn index() -> &'static str {
+#[get("")]
+async fn hello_world() -> &'static str {
     "Hello World! It's your Shield"
 }
 
-fn get_path(route: &RouteKey) -> String {
-    let routes_map = get_routes();
-    routes_map.get(route).map(|s|s.to_string()).expect("Route not found")
+#[shuttle_runtime::main]
+async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.service(scope("/").service(hello_world))
+            .service(scope("/organisations").service(organisation_factory()));
+    };
+
+    Ok(config.into())
 }
